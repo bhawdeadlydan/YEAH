0f811af0a844e8c6c3fa6ecffa37890e4e3cd2b4
Alessio <solinvictus.adc@gmail.com>
2015-09-28 22:42:09 +0200

    cleaned code and fix some bugs

--- src/docroot/yeah.js
+++ src/docroot/yeah.js
@@ -16,21 +16,47 @@
 
         var that=this;
 
-        $( window ).scroll(function(e) {
-            var _sp = $(window).scrollTop();
-            var objIntoView=$.fn.yeah.objsWebViewLocation(that,_sp);
+        this.getElementsPosition=function()
+        {
+            var _arr=[];
+            that.each(function(i,el){
+                var offset=$(el).offset().top, inOffset=$(el).data("yeah-animation-in-offset"), outOffset=$(el).data("yeah-animation-out-offset");
 
-            $(objIntoView.objIntoWebView).each(function(i,el){
+                var obj={
+                    'element':$(el),
+                    'bottom':parseInt(offset - window.innerHeight + $(el).outerHeight()),
+                    'top':parseInt(offset),
+                    'inOffset':inOffset ? parseInt(inOffset) : 0,
+                    'outOffset':outOffset ? parseInt(outOffset) : 0
+                };
+
+                _arr.push(obj);
+
+            });
+
+            return _arr;
+        }
+
+        this.animatedElements=this.getElementsPosition();
+
+        $( window ).scroll(function(e)
+        {
+            var _sp = parseInt($(window).scrollTop());
+
+            var objs=$.fn.yeah.objsWebViewLocation(that.animatedElements,_sp);
+
+            $(objs.objIntoWebView).each(function(i,el){
                 $(el).removeClass($(el).data("yeah-animation-out"));
                 $(el).addClass($(el).data("yeah-animation-in"));
             });
 
-            $(objIntoView.objOutWebView).each(function(i,el){
+            $(objs.objOutWebView).each(function(i,el){
                 $(el).removeClass($(el).data("yeah-animation-in"));
                 $(el).addClass($(el).data("yeah-animation-out"));
             });
 
         });
+
 
         return this;
 
@@ -41,17 +67,16 @@
     {
         var _arrInto=[];
         var _arrOut=[];
-        objList.each(function(i,el){
-            var objPosition=($(el).offset().top - window.innerHeight + $(el).outerHeight());
 
-            //if(objPosition <=_scrollPosition - $(el).data("yeah-animation-in-offset") && _scrollPosition<=objPosition+window.innerHeight-$(el).outerHeight()-$(el).data("yeah-animation-out-offset"))
-            if(objPosition <=_scrollPosition  && _scrollPosition<=objPosition+window.innerHeight-$(el).outerHeight())
+        $(objList).each(function(i,el){
+
+            if(el.bottom <=_scrollPosition - el.inOffset  && _scrollPosition + el.outOffset <= el.top)
             {
-                _arrInto.push(el);
+                _arrInto.push(el.element);
             }
             else
             {
-                _arrOut.push(el);
+                _arrOut.push(el.element);
             }
         });
 
